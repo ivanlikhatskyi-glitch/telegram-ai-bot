9751a6364f4c206a7e1fd5803496371289512d50
ivanlikhatskyi-glitch <ivanlikhatskyi@gmail.com>
2026-03-10 20:13:02 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,12 +6,12 @@ TOKEN = os.environ.get("BOT_TOKEN")
 
 app = Flask(__name__)
 
-def send_message(chat_id, text, reply_to=None):
-    url = f"https://api.telegram.org/bot{8202274177:AAH3CixiWuhRm-FxlHf7_8r2wdgVB3CzJKA}/sendMessage"
 
+def send_message(chat_id, text, reply_to=None):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
     data = {
         "chat_id": chat_id,
-        "text": text
+        "text": text,
     }
 
     if reply_to:
@@ -22,30 +22,26 @@ def send_message(chat_id, text, reply_to=None):
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
-
     data = request.json
 
     if "message" not in data:
         return "ok"
 
     message = data["message"]
-
     text = message.get("text", "")
     chat_id = message["chat"]["id"]
     message_id = message["message_id"]
 
-    # бот отвечает только если его упомянули
     if "@yourbotname" not in text.lower():
         return "ok"
 
-    question = text.lower().replace("@yourbotname", "").strip()
+    question = text.replace("@yourbotname", "").strip()
 
-    if question == "":
+    if not question:
         send_message(chat_id, "Напиши вопрос после упоминания 🙂", message_id)
         return "ok"
 
     answer = f"Ты написал: {question}"
-
     send_message(chat_id, answer, message_id)
 
     return "ok"
